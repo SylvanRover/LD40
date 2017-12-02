6cc624584bba6a9e9b45c7c051e36ea9aebe09bc
Denman Rooke <contact@denmanrooke.com>
2017-12-02 20:42:49 +0000

    Upgrading stations now changes sprite

--- Assets/Scripts/StationObject.cs
+++ Assets/Scripts/StationObject.cs
@@ -6,20 +6,29 @@ public class StationObject : MonoBehaviour {
 
 	public TechTree techTree;
 	private SpriteRenderer mySpriteRenderer;
+	public bool interactable = false;
+	public Sprite[] spriteLevels;
 
 	void Start(){
 		mySpriteRenderer = GetComponent<SpriteRenderer>();
+		SpriteLevel(0);
 	}
 
 	void OnTriggerEnter2D(Collider2D col){	
 		if(col.gameObject.tag == "Player") {
 			mySpriteRenderer.color = Color.red;
+			interactable = true;
 		}
 	}
 	void OnTriggerExit2D(Collider2D col){	
 		if(col.gameObject.tag == "Player") {
 			mySpriteRenderer.color = Color.white;
+			interactable = false;
 		}
+	}
+
+	public void SpriteLevel(int i){
+		mySpriteRenderer.sprite = spriteLevels[i];
 	}
 
 }
--- Assets/Scripts/TechTree.cs
+++ Assets/Scripts/TechTree.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Station {
@@ -8,12 +9,14 @@ public class Station {
 	public int level;
 	public int cost;
 	public GameObject target;
+	public bool interactable = false;
 
-	public Station(string newName, int newLevel, int newCost, GameObject newTarget) {
+	public Station(string newName, int newLevel, int newCost, GameObject newTarget, bool newInteractable) {
 		name = newName;
 		level = newLevel;
 		cost = newCost;
 		target = newTarget;
+		interactable = newInteractable;
 	}
 }
 
@@ -21,14 +24,38 @@ public class TechTree : MonoBehaviour {
 
 	public Station[] stations;
 	public float cash = 0;
+	public Text cashText;
+	private KeyCode buttonInteract;
+	public string cashTitle = "Cash: $";
 
 	void Start () {
-		
+		cashText.text = "Cash: $" + cash.ToString();
+		buttonInteract = KeyCode.Space;
+		for(int i = 0; i < stations.Length; ++i){
+			stations[i].interactable = stations[i].target.GetComponent<StationObject>().interactable;		
+		}
 	}
 
 	void UpgradeStation (int i) {
 		if (cash >= stations[i].cost){
 			cash = cash - stations[i].cost;
+			cashText.text = cashTitle + cash.ToString();
+			stations[i].level = stations[i].level + 1;
+			var stationObject = stations[i].target.GetComponent<StationObject>();
+			stationObject.SpriteLevel(stations[i].level);
+			Debug.Log(stationObject + " Level " + stations[i].level);
 		}
 	}
+
+	void Update(){
+		if (Input.GetKeyDown(buttonInteract)){
+			for(int i = 0; i < stations.Length; ++i){
+				stations[i].interactable = stations[i].target.GetComponent<StationObject>().interactable;
+				if (stations[i].interactable){
+					UpgradeStation(i);
+				}				
+			}
+		}
+	}
+
 }
