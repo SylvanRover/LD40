ae51b00cb7e627039416c4c1d19f5f0667efa7cd
Denman Rooke <contact@denmanrooke.com>
2017-12-02 21:12:44 +0000

    Negative profit in and working

--- Assets/Scripts/TechTree.cs
+++ Assets/Scripts/TechTree.cs
@@ -22,14 +22,16 @@ public class Station {
 
 public class TechTree : MonoBehaviour {
 
-	public Station[] stations;
 	public float cash = 0;
+	public string cashTitle = "PROFIT";
+	public Text profitText;
 	public Text cashText;
+	public Color negativeProfitColor;
 	private KeyCode buttonInteract;
-	public string cashTitle = "Cash: $";
+	public Station[] stations;
 
 	void Start () {
-		cashText.text = "Cash: $" + cash.ToString();
+		cashText.text = cash.ToString();
 		buttonInteract = KeyCode.Space;
 		for(int i = 0; i < stations.Length; ++i){
 			stations[i].interactable = stations[i].target.GetComponent<StationObject>().interactable;		
@@ -37,14 +39,21 @@ public class TechTree : MonoBehaviour {
 	}
 
 	void UpgradeStation (int i) {
-		if (cash >= stations[i].cost){
-			cash = cash - stations[i].cost;
-			cashText.text = cashTitle + cash.ToString();
-			stations[i].level = stations[i].level + 1;
-			var stationObject = stations[i].target.GetComponent<StationObject>();
-			stationObject.SpriteLevel(stations[i].level);
-			Debug.Log(stationObject + " Level " + stations[i].level);
+		cash = cash - stations[i].cost;
+		if (cash < 0){
+			cashText.text = "-$" + Mathf.Abs(cash).ToString();
+			cashText.color = negativeProfitColor;
+		} else if (cash == 0){
+			cashText.text = "$" + Mathf.Abs(cash).ToString();
+			cashText.color = negativeProfitColor;
+		} else {
+			cashText.text = "$" + Mathf.Abs(cash).ToString();
 		}
+		profitText.text = cashTitle + ": ";
+		stations[i].level = stations[i].level + 1;
+		var stationObject = stations[i].target.GetComponent<StationObject>();
+		stationObject.SpriteLevel(stations[i].level);
+		Debug.Log(stationObject + " Level " + stations[i].level);
 	}
 
 	void Update(){
