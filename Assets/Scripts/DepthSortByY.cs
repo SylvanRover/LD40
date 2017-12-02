229037ad7e35a308201abfce9a293710bcdfb384
Denman Rooke <contact@denmanrooke.com>
2017-12-02 18:22:07 +0000

    Getting basic collision detection in

---
+++ Assets/Scripts/DepthSortByY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class DepthSortByY : MonoBehaviour
+{
+
+    private const int IsometricRangePerYUnit = 100;
+
+    void Update()
+    {
+        Renderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sortingOrder = -(int)(transform.position.y * IsometricRangePerYUnit);
+    }
+}
---
+++ Assets/Scripts/StationObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationObject : MonoBehaviour {
+
+	public TechTree techTree;
+	private SpriteRenderer mySpriteRenderer;
+
+	void Start(){
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col){	
+		if(col.gameObject.tag == "Player") {
+			mySpriteRenderer.color = Color.red;
+		}
+	}
+	void OnTriggerExit2D(Collider2D col){	
+		if(col.gameObject.tag == "Player") {
+			mySpriteRenderer.color = Color.white;
+		}
+	}
+
+}
